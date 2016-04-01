790d28a26c88a1b880b12fc3f89a55ac702e841c
Jayme <liujiescut@163.com>
2016-04-02 00:06:32 +0800

    完成全部UI ！！！

--- app/src/main/java/com/scut/easyfe/ui/activity/EvaluationActivity.java
+++ app/src/main/java/com/scut/easyfe/ui/activity/EvaluationActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.scut.easyfe.R;
+import com.scut.easyfe.app.Constants;
 import com.scut.easyfe.ui.base.BaseActivity;
 import com.scut.easyfe.utils.OtherUtils;
 
@@ -21,5 +22,23 @@ public class EvaluationActivity extends BaseActivity {
         ((TextView) OtherUtils.findViewById(this, R.id.titlebar_tv_title)).setText("已完成订单");
         ((TextView) OtherUtils.findViewById(this, R.id.titlebar_tv_right)).setText("保存修改");
         ((TextView) OtherUtils.findViewById(this, R.id.titlebar_tv_right)).setVisibility(View.VISIBLE);
+    }
+
+    public void onBackClick(View view){
+        finish();
+    }
+
+    public void onRightClick(View view){
+
+    }
+
+    public void onComplaintClick(View view){
+        Bundle bundle = new Bundle();
+        bundle.putInt(Constants.Key.CALLBACK_TYPE, Constants.Identifier.CALLBACK_COMPLAINTS);
+        redirectToActivity(this, CallbackActivity.class, bundle);
+    }
+
+    public void onMoneyTicketClick(View view){
+
     }
 }
