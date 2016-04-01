c9bc39c9f2d5726dbe4812a45d9ed30502de76b7
Jayme <liujiescut@163.com>
2016-04-01 11:27:14 +0800

    家长评语UI 跟一些小Bug修复

--- app/src/main/java/com/scut/easyfe/ui/activity/CommentsActivity.java
+++ app/src/main/java/com/scut/easyfe/ui/activity/CommentsActivity.java
@@ -1,16 +1,49 @@
 package com.scut.easyfe.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.scut.easyfe.R;
+import com.scut.easyfe.entity.ToSelectItem;
+import com.scut.easyfe.ui.adapter.SelectItemAdapter;
+import com.scut.easyfe.ui.base.BaseActivity;
+import com.scut.easyfe.utils.DensityUtil;
+import com.scut.easyfe.utils.OtherUtils;
 
-public class CommentsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class CommentsActivity extends BaseActivity {
+    private ListView mCommentListView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void setLayoutView() {
         setContentView(R.layout.activity_comments);
-        // Todo
+    }
+
+    @Override
+    protected void initView() {
+        ((TextView)findViewById(R.id.titlebar_tv_title)).setText("家长评语");
+
+        mCommentListView = OtherUtils.findViewById(this, R.id.comments_lv_container);
+        mCommentListView.setDividerHeight(DensityUtil.dip2px(this, 5));
+        mCommentListView.setHeaderDividersEnabled(true);
+
+        View headView= new View(this);
+        headView.setBackground(null);
+        mCommentListView.addHeaderView(headView);
+
+        ArrayList<ToSelectItem> comments = new ArrayList<>();
+        for(int i = 0; i < 25; i++){
+            String comment = "\n杨 * 杰 先生  2016-3-8  5：20 \n\n习惯好评   " + i +"\n";
+            comments.add(new ToSelectItem(comment, false));
+        }
+        SelectItemAdapter adapter = new SelectItemAdapter(mContext, comments);
+        adapter.setSelectable(false);
+        mCommentListView.setAdapter(adapter);
+    }
+
+    public void onBackClick(View view){
+        finish();
     }
 }
--- app/src/main/java/com/scut/easyfe/ui/fragment/SearchResultFragment.java
+++ app/src/main/java/com/scut/easyfe/ui/fragment/SearchResultFragment.java
@@ -9,33 +9,45 @@ import android.widget.TextView;
 import com.scut.easyfe.R;
 import com.scut.easyfe.app.Constants;
 import com.scut.easyfe.entity.Order;
+import com.scut.easyfe.ui.activity.ConfirmOrderActivity;
 import com.scut.easyfe.ui.activity.TeacherInfoActivity;
 import com.scut.easyfe.ui.adapter.SearchResultAdapter;
 import com.scut.easyfe.ui.base.BaseRefreshFragment;
 import com.scut.easyfe.utils.DensityUtil;
+import com.scut.easyfe.utils.DialogUtils;
+import com.scut.easyfe.utils.LogUtils;
+import com.scut.easyfe.utils.MapUtils;
+
+import java.util.List;
 
 /**
  * 单次预约多次预约筛选结果页
  * Created by jay on 16/3/31.
  */
-public class SearchResultFragment extends BaseRefreshFragment{
+public class SearchResultFragment extends BaseRefreshFragment {
     private int mReserveType = Constants.Identifier.RESERVE_MULTI;
+    private List<Order> mOrders;
+
+    @Override
+    protected void initData() {
+        mOrders = Order.getTestOrders();
+    }
 
     @Override
     protected void initView(View view) {
         super.initView(view);
 
-        if(null == getActivity()){
+        if (null == getActivity()) {
             return;
         }
 
         int mOrderNum = 4;
 
-        TextView headView= new TextView(getActivity());
+        TextView headView = new TextView(getActivity());
         headView.setMinimumHeight(DensityUtil.dip2px(mActivity, 5));
-        if(mOrderNum > 5) {
+        if (mOrderNum > 5) {
             headView.setBackground(null);
-        }else{
+        } else {
             headView.setBackgroundResource(R.color.search_result_tip_bg);
             headView.setGravity(Gravity.CENTER);
             headView.setTextColor(getResources().getColor(R.color.text_hint_color));
@@ -45,7 +57,7 @@ public class SearchResultFragment extends BaseRefreshFragment{
 
         mDataListView.addHeaderView(headView);
         mDataListView.setDividerHeight(DensityUtil.dip2px(mActivity, 5));
-        mAdapter = new SearchResultAdapter(getActivity(), Order.getTestOrders());
+        mAdapter = new SearchResultAdapter(getActivity(), mOrders);
         setBaseAdapter(mAdapter);
     }
 
@@ -61,19 +73,49 @@ public class SearchResultFragment extends BaseRefreshFragment{
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if(null == mActivity){
+        if (null == mActivity) {
             return;
         }
 
         //点击HeadView
-        if(position == 0){
+        if (position == 0) {
             return;
         }
 
-        Bundle bundle = new Bundle();
-        bundle.putInt(Constants.Key.RESERVE_WAY, getReserveType());
-        bundle.putSerializable(Constants.Key.ORDER, Order.getTestOrders().get(position - 1));
-        mActivity.redirectToActivity(mActivity, TeacherInfoActivity.class, bundle);
+        final int index = position - 1;   //减1是为了减去HeaderView
+        MapUtils.getDurationFromPosition(mOrders.get(index).getTeacherLatitude(), mOrders.get(index).getTeacherLongitude(),
+                mOrders.get(index).getParentLatitude(), mOrders.get(index).getParentLongitude(),
+                mOrders.get(index).getCity(), new MapUtils.GetDurationCallback() {
+                    @Override
+                    public void onSuccess(int durationSeconds) {
+                        if (null == mActivity) {
+                            return;
+                        }
+
+                        if (durationSeconds / 60 > mOrders.get(index).getTeacherMaxAcceptTime()) {
+                            DialogUtils.makeConfirmDialog(mActivity, "温馨提示", "您与家教老师的距离已超过他（她）设定的最远距离，试试别的老师吧。");
+                        } else {
+                            if (durationSeconds / 60 < mOrders.get(index).getTeacherAcceptTime()) {
+                                mOrders.get(index).setTip(0);
+                            }
+
+                            Bundle bundle = new Bundle();
+                            bundle.putInt(Constants.Key.RESERVE_WAY, getReserveType());
+                            bundle.putSerializable(Constants.Key.ORDER, mOrders.get(index));
+                            mActivity.redirectToActivity(mActivity, TeacherInfoActivity.class, bundle);
+                        }
+                    }
+
+                    @Override
+                    public void onFailed(String errorMsg) {
+                        LogUtils.i(Constants.Tag.MAP_TAG, errorMsg);
+
+                        if (null == mActivity) {
+                            return;
+                        }
+                        DialogUtils.makeConfirmDialog(mActivity, "温馨提示", "您与家教老师的距离已超过他（她）设定的最远距离，试试别的老师吧。");
+                    }
+                });
     }
 
     public int getReserveType() {
